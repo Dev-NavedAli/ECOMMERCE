31218e817a2aa97436d9d33505cfa14b7a777aac
Naved khan <nomanali50997@gmail.com>
2024-09-12 10:46:51 +0530

    styling the navbar in small screens

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const [visible,setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
@@ -45,6 +46,22 @@ const Navbar = () => {
           10
         </p>
         </Link>
+        <img  onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
+      </div>
+      {/* sidebar menu for small screen  */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all  ${visible ? 'w-full' : 'w-0'}`}>
+         <div className='flex flex-col text-gray-600 '>
+          <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+            <img src={assets.dropdown_icon} alt="" className='h-4 rotate-180' />
+            <p >Back</p>
+          </div>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>Home</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>Collection</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>About</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>Contact</NavLink>
+
+          
+         </div>
       </div>
     </div>
   )
