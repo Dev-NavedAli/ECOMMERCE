7cc5a384ec4c4c2e3816546700d9afc19729a4bc
Naved khan <nomanali50997@gmail.com>
2024-09-14 09:52:55 +0530

    redirecting to home page using logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
-      <img src={assets.logo} alt="" className='w-36' />
+     <Link to='/'> <img src={assets.logo} alt="" className='w-36' /></Link>
 
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
 
