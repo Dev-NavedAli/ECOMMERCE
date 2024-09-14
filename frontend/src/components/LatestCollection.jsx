d8c4f5e4a877b066c5acda61ca7ae10994aa726b
Naved khan <nomanali50997@gmail.com>
2024-09-14 09:45:59 +0530

    working of rendering the image

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Tittle from './Tittle'
+import ProductItem from './ProductItem'
 
 const LatestCollection = () => {
     const {products} = useContext(ShopContext)
@@ -12,7 +13,13 @@ const LatestCollection = () => {
     <div className='my-10'>
         <div className='text-center py-8 text-3xl'>
            <Tittle text1={'LATEST'} text2={'COLLECTION'}/>
-           <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusamus asperiores aspernatur error magni itaque vel beatae cupiditate placeat, quidem et ex hic ipsa, assumenda quia similique? Vel adipisci consectetur officia distinctio maiores. Quasi earum nulla eligendi quaerat adipisci vitae.</p>
+           <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusamus asperiores aspernatur  sci vitae.</p>
+        </div>
+        {/* Rendering profducts */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+          {
+            latestProducts.map((item,index)=>(<ProductItem  key={index} id={item._id} image={item.image} name={item.name} price={item.price}  />))
+          }
         </div>
       
     </div>
--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -2,13 +2,15 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom'
 
-const ProductItem = ({id,name,price}) => {
+const ProductItem = ({id,name,price,image}) => {
     const {currency} = useContext(ShopContext)
   return (
     <Link to={`/product/${id}`} className='text-gray700 cursor-pointer'>
         <div className='overflow-hidden'>
-            <img src="" alt="" />
+            <img src={image[0]} alt="" className='hover:scale-110 transition ease-in-out' />
         </div>
+        <p className='pt-3 pb-1 text-sm'>{name}</p>
+        <p className='text-sm font-medium' >{currency}{price}</p>
 
     </Link>
   )
