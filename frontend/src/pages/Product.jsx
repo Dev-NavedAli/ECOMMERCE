e5a3d9c5bbb499f827808d472703c5754d15e4b9
Naved khan <nomanali50997@gmail.com>
2024-09-18 18:33:35 +0530

    display information

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
+import { assets } from '../assets/assets';
 
 const product = () => {
   const { productId } = useParams();
-  const { products } = useContext(ShopContext)
+  const { products,currency } = useContext(ShopContext)
   const [productData, setProductData] = useState(false)
   const [image,setImage] = useState('')
 
@@ -12,7 +13,6 @@ const product = () => {
      products.map((item)=>{
       if(item._id == productId){
         setProductData(item)
-        console.log(item)
         setImage(item.image[0])  
         return null;
       }
@@ -39,7 +39,30 @@ const product = () => {
           <div className="w-full sm:w-[80%]">
             <img src={image} className='w-full  h-auto' alt=""/>
           </div>
+         </div>
 
+         {/* ----------------------------- Product Information------------- */}
+         <div className="flex-1">
+          <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
+          <div className='flex items-center gap-1 mt-2'>
+            <img src={assets.star_icon} alt="" className="w-3.5" />
+            <img src={assets.star_icon} alt="" className="w-3.5" />
+            <img src={assets.star_icon} alt="" className="w-3.5" />
+            <img src={assets.star_icon} alt="" className="w-3.5" />
+            <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+            <p className='pl-2'>(122)</p>
+          </div>
+          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <p className='mt-5 text-gray-500 md:w-4/5  '>{productData.description}</p>
+          <div className='flex flex-col gap-4 my-8'>
+            <p>Selec Size</p>
+            <div className='flex gap-2'>
+              {productData.sizes.map((item,index)=>(
+                <button key={index} className={`border py-2 px-4 bg-gray-100`} >{item}</button>
+              ))}
+
+            </div>
+          </div>
          </div>
       </div>
     </div>
