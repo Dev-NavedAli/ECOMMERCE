55b35b3892356d880b232a3eea8679cf7f7a3297
Naved khan <nomanali50997@gmail.com>
2024-09-18 13:32:33 +0530

    xyz

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 const product = () => {
+  const {productId}  = useParams();
+  useEffect(()=>console.log(productId))
   return (
     <div>
       
