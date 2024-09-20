c6a8a99be641de40b5a69b5bbf3618713e1f8cf7
Naved khan <nomanali50997@gmail.com>
2024-09-20 11:54:35 +0530

    making the ui

--- frontend/src/pages/Placeorder.jsx
+++ frontend/src/pages/Placeorder.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Tittle from '../components/Tittle'
 import CartTotal from '../components/CartTotal'
 import { assets } from '../assets/assets'
+import { ShopContext } from '../context/ShopContext'
 
 const Placeorder = () => {
   const [method,setMethod]  = useState('cod');
+  const {navigate} =  useContext(ShopContext)
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h[80vh] border-t'>
       {/*--------------- Left side-------------------- */}
@@ -48,6 +50,10 @@ const Placeorder = () => {
                 <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' :''} `}></p>
                 <p className='text-gray-500 text-sm font-medium mx-4 '>CASH ON DELIVERY</p>
               </div>
+            </div>
+            <div className='w-full text-end mt-8'>
+              <button onClick={()=>navigate('/orders')} className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
+
             </div>
           </div>
 
