3cd8f6e38a5561c7089d17de1960865210a5d932
Naved khan <nomanali50997@gmail.com>
2024-09-20 12:57:30 +0530

    make changes in aboutus.jsx

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import Tittle from '../components/Tittle'
+import { assets } from '../assets/assets'
+import NewsLetterBox from "../components/NewsLetterBox"
 
 const about = () => {
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Tittle text1={"ABOUT"} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} alt="" className='w-full md:max-w-[450px]' />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates sunt asperiores dolores doloremque debitis ipsa animi soluta quos nemo totam hic illum alias suscipit distinctio tempore quam porro, exercitationem quasi.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quo natus quas, rem molestiae sapiente cum ut harum tenetur? Voluptates earum atque sequi aliquam soluta libero quas laboriosam pariatur vero.</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio aliquid voluptatum consequatur sunt natus commodi ullam ea quia corrupti velit modi nostrum soluta laborum excepturi hic dolorum quidem, asperiores dolor.</p>
+        </div>
+      </div>
+      <div className='text-xl py-4'>
+        <Tittle text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20 gap-2'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance : </b>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus expedita ea voluptas accusantium corporis atque eligendi ad neque assumenda. Pariatur temporibus nesciunt corrupti esse recusandae officia iusto dolorum cumque consequuntur?</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convinience : </b>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus expedita ea voluptas accusantium corporis atque eligendi ad neque assumenda. Pariatur temporibus nesciunt corrupti esse recusandae officia iusto dolorum cumque consequuntur?</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer service : </b>
+          <p className='text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus expedita ea voluptas accusantium corporis atque eligendi ad neque assumenda. Pariatur temporibus nesciunt corrupti esse recusandae officia iusto dolorum cumque consequuntur?</p>
+        </div>
+      </div>
+      <NewsLetterBox/>
+
+
     </div>
+
+
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,8 +2,14 @@ import React, { useState } from 'react'
 
 const login = () => {
   const [currentState, setCurrentState] = useState('Sign Up')
+
+  const onSubmitHandler  = (e)=>{
+    e.preventDefault()
+  }
+
   return (
-    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className='prata-regular text-3xl'> {currentState}</p>
         <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
@@ -19,6 +25,7 @@ const login = () => {
             : <p onClick={() => setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
         }
       </div>
+      <button  className='bg-black text-white font-light px-8 py-2 mt-4'>{currentState === "Login" ? 'Sign In':'Sign Up'}</button>
 
     </form>
 
