f442865e8b1e553679bd4eae7164acc8891ed64a
Naved khan <nomanali50997@gmail.com>
2024-09-22 10:21:09 +0530

    making routes for login, signup and admin login

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,23 @@
+// Route for User Login 
+const loginUser = async(req,res)=>{
+
+}
+
+
+// Route for User Registeration
+
+const registerUser = async(req,res)=>{
+    res.json({message:"Register api working properly"})
+
+}
+
+// Route for Admin Login
+
+const adminLogin = async(req,res)=>{
+
+}
+
+
+
+
+export {loginUser,registerUser,adminLogin}
--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import dotenv from "dotenv"
 import connectDb from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js"
+import userRouter from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ app.use(express.json())
 app.use(cors())
 
 //Api End points
+
+app.use('/api/user',userRouter)
 
 app.get("/",(req,res)=>{
     res.send("Hi there")
