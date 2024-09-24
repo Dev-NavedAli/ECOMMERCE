04b34bfe61d48c96badd1dae1d904acfa825aa86
Naved khan <nomanali50997@gmail.com>
2024-09-24 14:24:57 +0530

    make the middleware that check the admin and apply this middleware for some route

---
+++ Backend/middleware/adminAuth.js
@@ -0,0 +1,21 @@
+// we will add this middleware for those api where we need the admin permision like:addproduct,removeproduct etc
+
+import jwt from "jsonwebtoken"
+
+const adminAuth = async(req,res,next)=>{
+    try {
+        const {token} = req.headers
+        if (!token) {
+            return res.json({success:false,message:"Not Authorized Login Again"})
+        }
+        const token_decode = jwt.verify(token,process.env.JWT_SECRET)
+        if(token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD){
+            return res.json({success:false,message:"Not Authorized Login Again"})
+        }
+        next()
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+export default adminAuth;
