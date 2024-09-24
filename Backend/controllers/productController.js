57278ff823ac5181f614d5d76bfb456fb97d33b0
Naved khan <nomanali50997@gmail.com>
2024-09-24 13:14:53 +0530

    making remove product api

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -46,7 +46,15 @@ const listProduct = async (req, res) => {
 };
 
 // Function for remove product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:"product removed"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:flase,message:error.message})
+    }
+};
 
 // Function for single product info
 const singleProduct = async (req, res) => {};
