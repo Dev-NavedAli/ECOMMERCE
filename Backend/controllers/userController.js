5f4f6320f8adaef798ccba4ab2463cc046949db8
Naved khan <nomanali50997@gmail.com>
2024-09-24 13:50:35 +0530

    making api Function for admin login

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -75,6 +75,21 @@ const registerUser = async (req, res) => {
 
 // Route for Admin Login
 
-const adminLogin = async (req, res) => { };
+const adminLogin = async (req, res) => { 
+    try {
+        const {email,password} = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
+        } else{
+            res.json({success:false,message:"Invalid Credentials"})
+        }         
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+
+};
 
 export { loginUser, registerUser, adminLogin };
