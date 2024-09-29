49ab3c12f95d271951429883ea16d862317c5d97
Naved khan <nomanali50997@gmail.com>
2024-09-29 13:43:30 +0530

    updating order status using axios

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,83 +1,84 @@
-import orderModel from "../models/orderModel.js"
-import userModel from "../models/userModel.js"
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
 
 // Placing order using cod
 
-const placeOrder = async(req,res)=>{
-    try {
-        const { userId , items , amount , address } = req.body;
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
 
-        const orderData = {
-            userId,
-            items,
-            amount,
-            address,
-            paymentMethod:"COD",
-            payment:false,
-            date:Date.now(),
-        }
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
 
-        const newOrder = new orderModel(orderData)
-        await newOrder.save()
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
 
-        await userModel.findByIdAndUpdate(userId,{cartData:{}}) //yha pr hmne order placed hone ke baad cart ko empty kiya hai
-        res.json({success:true,message:"Order placed"})
-
-    } catch (error) {
-        console.log(error);
-        res.json({success:false,message:error.message})
-        
-    }
-
-}
-
+    await userModel.findByIdAndUpdate(userId, { cartData: {} }); //yha pr hmne order placed hone ke baad cart ko empty kiya hai
+    res.json({ success: true, message: "Order placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Placing order using Stripe Method
 
-const placeOrderStripe = async(req,res)=>{
-
-}
-
+const placeOrderStripe = async (req, res) => {};
 
 // Placing order using razorpay method
 
-const placeOrderRazorpay = async(req,res)=>{
-
-}
+const placeOrderRazorpay = async (req, res) => {};
 
 //All Orders data for Admin Panel
 
-const allOrders = async(req,res)=>{
-    try {
-        const orders = await orderModel.find({})
-        res.json({success:true,orders})
-    } catch (error) {
-        console.log(error);
-        res.json({success:false,message:error.message})
-    }
-
-}
+const allOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //User Orders data for Frontend
 
-const userOrders = async(req,res)=>{
-    try {    
-        const { userId } = req.body
-        const orders = await orderModel.find({ userId })
-        res.json({sucess:true,orders})
+const userOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const orders = await orderModel.find({ userId });
+    res.json({ sucess: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-    } catch (error) {
-        console.log(error);
-        res.json({success:false,message:error.message})
-    }
-    
-}
+//update order Status from admin Panel
 
+const updateStatus = async (req, res) => {
+  try {
+    const { orderId, status } = req.body;
+    await orderModel.findByIdAndUpdate(orderId, { status });
+    res.json({ sucess: true, message: "Status Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-//update order Status from admin
-
-const updateStatus = async(req,res)=>{
-    
-}
-
-export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus}
+export {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+};
--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -26,6 +26,23 @@ const Orders = ({ token }) => {
     }
   }
 
+
+  const statusHandler = async( event , orderId ) => {
+    try {
+      const response = await axios.post(backendUrl+'/api/order/status',{orderId,status:event.target.value},{headers:{token}})
+      console.log(response);
+      
+      if (response.data.sucess) {
+        await fetchAllOrders()
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(response.data.message)
+      
+      
+    }
+  }
+
   useEffect(() => {
     fetchAllOrders();
   }, [token])
@@ -37,21 +54,21 @@ const Orders = ({ token }) => {
       <div>
         {
           orders.map((order, index) => (
-            <div key={index}>
-              <img src={assets.parcel_icon} alt="" />
+            <div className='grid grid-cols-1 sm:grid-cols-[0.5fr_2fr_1fr] lg:grid-cols-[0.5fr_2fr_1fr_1fr_1fr] gap-3 items-start border border-gray-400 p-5 md:p-8 my-3 md:my-4 text-xs sm:text-sm text-gray-700 ' key={index}>
+              <img className='w-12' src={assets.parcel_icon} alt="" />
               <div>
                 <div>
                   {
                     order.items.map((item, index) => {
                       if (index === order.items.length - 1) {
                         {/*  Yeh condition tabhi true hogi jab tum last item par ho, kyunki index will equal order.items.length - 1 sirf last item ke liye.   */ }
-                        return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span> </p>
+                        return <p className='py-0.5 ' key={index}>{item.name} x {item.quantity} <span>{item.size}</span> </p>
                       } else {
-                        return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span>, </p>
+                        return <p className='py-0.5 ' key={index}>{item.name} x {item.quantity} <span>{item.size}</span>, </p>
                       }
                     })}
                 </div>
-                <p>{order.address.firstName + "" + order.address.lastName}</p>
+                <p className='mt-3 mb-2 font-medium'>{order.address.firstName + "" + order.address.lastName}</p>
                 <div>
                   <p>{order.address.street + ","}</p>
                   <p>{order.address.city + " , " + order.address.state + ", " + order.address.country + " , " + order.address.zipcode + " , "}</p>
@@ -59,17 +76,17 @@ const Orders = ({ token }) => {
                 <p>{order.address.phone}</p>
               </div>
               <div>
-                <p>Itmes : {order.items.length}</p>
-                <p>Method : {order.paymentMethod}</p>
-                <p>Payment : {order.payment ? 'Done' : 'Pending'}</p>
+                <p className='text-sm sm:text-[15px]'>Itmes : {order.items.length}</p>
+                <p className='mt-3'>Method : {order.paymentMethod}</p>
+                <p>Payment : {order.payment ? 'Done' : 'Pending' }</p>
                 <p>Date {new Date(order.date).toLocaleDateString()}</p>
               </div>
-              <p>{currency}{order.amount}</p>
-              <select>
+              <p className='text-sm sm:text-[15px]'>{currency}{order.amount}</p>
+              <select onChange={(event)=> statusHandler(event,order._id)} value={order.status} className='p-2 font-semibold'>
                 <option value="Order Placed">Order Placed</option>
                 <option value="Packing"> Packing</option>
                 <option value="Shipped">Shipped</option>
-                <option value="Out For Delivery">Out For Delivery</option>
+                <option value="Out for delivery">Out for delivery</option>
                 <option value="Delivered">Delivered</option>
               </select>
             </div>
