339ac4d1c616e77238337752b32b34335c200251
Naved khan <nomanali50997@gmail.com>
2024-10-01 12:35:50 +0530

    add some more origins

--- Backend/server.js
+++ Backend/server.js
@@ -20,12 +20,15 @@ connectCloudinary
 
 //middlewares
 app.use(express.json())
-app.use(cors())
   
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
+const corsOptions = {
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  };
+  
+  app.use(cors(corsOptions));
+  
 //Api End points
 
 app.use('/api/user',userRouter)
