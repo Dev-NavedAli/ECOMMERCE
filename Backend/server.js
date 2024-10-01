25378e8129c73f74635a1567728d54e34581049e
Naved khan <nomanali50997@gmail.com>
2024-10-01 17:11:06 +0530

    final commit for admin

--- Backend/server.js
+++ Backend/server.js
@@ -20,11 +20,31 @@ connectCloudinary
 
 //middlewares
 app.use(express.json())  
-const corsOptions = {
-    origin: 'https://ecommerce-frontend-eight-xi.vercel.app',
+
+
+const allowedOrigins = [
+    'https://ecommerce-frontend-eight-xi.vercel.app',
+    'https://ecommerce-admin-panel-chi-ecru.vercel.app' // Replace with your second URL
+  ];
+  
+  const corsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error('Not allowed by CORS')); // Reject the request
+      }
+    },
   };
   
-app.use(cors(corsOptions));
+  app.use(cors(corsOptions))
+
+
+// const corsOptions = {
+//     origin: 'https://ecommerce-frontend-eight-xi.vercel.app',
+//   };
+  
+// app.use(cors(corsOptions));
 
   
   
